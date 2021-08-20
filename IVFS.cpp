604abd31806742520e8086a686b6e1dc2695e8f9
bogol243 <bogol243@gmail.com>
2021-08-20 15:09:00 +0300

    Directory -- added AddFile() and FindRecord() methods

--- IVFS.cpp
+++ IVFS.cpp
@@ -422,6 +422,32 @@ public:
 		: _fd(fd) {
 
 	}
+
+	/* Добавить файл в директорию
+	* File* fd			-- файловый дескриптор
+	* std::string name	-- имя добавляемого файла
+	*/
+	void AddFile(File* new_file_fd, std::string name) {
+		DirRecord new_record;
+		new_record.inode_id = new_file_fd->inode_id;
+		std::memcpy(new_record.name, name.c_str(), name.size()+1 < 16 ? name.size()+1 : 16);
+		ds.Append(_fd, reinterpret_cast<char*>(&new_record), sizeof(DirRecord));
+		
+	}
+
+	DirRecord* FindRecord(char name[16]) {
+		DirRecord* dir_record = new DirRecord;
+
+		ds.Read(_fd, reinterpret_cast<char*>(dir_record), sizeof(DirRecord));
+		
+		if (std::strcmp(dir_record->name, name) == 0) {
+			return dir_record;
+		}
+		delete dir_record;
+		return nullptr;
+	}
+
+	friend std::ostream& operator<< (std::ostream& out, Directory dir);
 };
 
 //class Directories {
